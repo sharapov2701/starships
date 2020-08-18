4957b9a944a42acd61b8c5031202a41e85ce88b6
sharapov2701 <50914537+sharapov2701@users.noreply.github.com>
2020-08-18 16:37:54 +0300

    gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename={'/starships'}>
                 <App />
             </BrowserRouter>
         </Provider>
